6c67c3f7c666b6ba9009e63f713d3edb9b1bad91
Ahmed Mohsin <67206945+moohsen07@users.noreply.github.com>
2022-05-23 01:36:37 +0200

    Time: 83 ms (43.51%), Space: 42.2 MB (76.34%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
@@ -3,26 +3,14 @@
  * @return {number}
  */
 
-// 1-loop through elements
-//     2-check if loop has element greater than nums[i]
-//         1-if true check if loop has element smaller than nums[i] 
-//             1- if true count++
-//         2- if false continue the loop 
-    
 var countElements = function(nums) {
-    let strictly = []
-    for(let i = 0; i < nums.length; i++){
-        strictly.push({num : nums[i] , hasGreater : false , hasSmaller : false})
-        for(let j = 0; j < nums.length ; j++){
-            if(nums[j] > nums[i] || nums[j] < nums[i]){
-                if(nums[j] > nums[i]){
-                    strictly[i].hasGreater = true
-                }
-                if(nums[j] < nums[i]){
-                    strictly[i].hasSmaller = true
-                }
-            }
-        }
-    }
-    return strictly.filter(s => s.hasGreater && s.hasSmaller).length 
+    let a = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] !== Math.max(...nums) && nums[i] !== Math.min(...nums)
+      ? a++
+      : (a = a);
+  }
+
+  return a;
 };
