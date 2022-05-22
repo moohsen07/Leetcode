4df4ecfc71ecdca4cb2a5badcaa1d97a45c353c2
Ahmed Mohsin <67206945+moohsen07@users.noreply.github.com>
2022-05-23 01:38:24 +0200

    Time: 87 ms (32.82%), Space: 43.7 MB (19.08%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
@@ -4,13 +4,19 @@
  */
 
 var countElements = function(nums) {
-    let a = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    nums[i] !== Math.max(...nums) && nums[i] !== Math.min(...nums)
-      ? a++
-      : (a = a);
-  }
-
-  return a;
+    let strictly = []
+    for(let i = 0; i < nums.length; i++){
+        strictly.push({num : nums[i] , hasGreater : false , hasSmaller : false})
+        for(let j = 0; j < nums.length ; j++){
+            if(nums[j] > nums[i] || nums[j] < nums[i]){
+                if(nums[j] > nums[i]){
+                    strictly[i].hasGreater = true
+                }
+                if(nums[j] < nums[i]){
+                    strictly[i].hasSmaller = true
+                }
+            }
+        }
+    }
+    return strictly.filter(s => s.hasGreater && s.hasSmaller).length 
 };
