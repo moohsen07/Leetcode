7223e2141bc2c7cc282aa390975181afb18712c2
Ahmed Mohsin <67206945+moohsen07@users.noreply.github.com>
2022-05-24 18:20:06 +0200

    Time: 98 ms (19.04%), Space: 43.5 MB (7.01%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.includes(target))
+        return binarySearch(nums , target , 0 , nums.length -1)
+    else {
+        nums.push(target)
+        const sortedArray = nums.sort((a,b) => a - b)
+        return binarySearch(sortedArray , target , 0 , nums.length - 1)
+    }
+};
+
+const binarySearch = (nums,target,start,end) => {
+    if(start > end) return false
+    let mid = Math.floor((start + end) / 2)
+    if(nums[mid] === target) return mid
+    else if(target > nums[mid]) return binarySearch(nums , target , mid + 1 , end)
+    else return binarySearch(nums , target , start , mid - 1)
+}
