fd14dc384f8470fa23e49b9d151a94482ab04714
Ahmed Mohsin <67206945+moohsen07@users.noreply.github.com>
2022-05-26 19:52:15 +0200

    Time: 95 ms (26.00%), Space: 42 MB (83.89%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,7 +4,35 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    matrix = matrix.flat()
-    const bol = matrix.includes(target) ? true : false
-    return bol
+    let start = 0 , 
+        end = matrix.length - 1 , 
+        cols = matrix[0].length , 
+        mid = start + Math.floor((end - start) /  2);
+    
+    while(matrix[mid][0] > target || matrix[mid][cols - 1] < target){
+        mid = start + Math.floor((end - start) /  2)
+        if(matrix[mid][0] > target)
+            end = mid - 1;
+        else if(matrix[mid][cols - 1] < target)
+            start = mid + 1;
+        if(start > end) return false;
+    }
+    return binarySearch(matrix[mid] ,target)
 };
+
+let binarySearch = (arr , target ) => {
+    let start = 0;
+    let end = arr.length - 1;
+    
+    while (start <= end){
+        let mid = start + Math.floor((end - start) / 2);
+        if (arr[mid] === target){
+            return true;
+        } else if (arr[mid] > target){
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return false
+}
