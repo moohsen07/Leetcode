44878e80485758c1b51f1bdcf98202d94fd4c642
Ahmed Mohsin <67206945+moohsen07@users.noreply.github.com>
2022-06-03 02:22:57 +0200

    Time: 136 ms (8.10%), Space: 44.8 MB (44.30%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.js
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.js
@@ -5,7 +5,6 @@
 var findClosestNumber = function(nums) {
     let min = nums[0]
     for(let num of nums){
-        console.log(num)
         if(Math.abs(num) === Math.abs(min)) min = Math.max(num , min)
         else if(Math.abs(num) < Math.abs(min)) min = num
     }
